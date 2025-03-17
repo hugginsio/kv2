a79fac75de50a54d9229dc056a026f79a8fd760c
Kyle Huggins <kyle@huggins.io>
2025-03-16 20:48:53 -0500

    feat: run container build & publish on release

--- dagger/cd.go
+++ dagger/cd.go
@@ -12,6 +12,7 @@ func (m *Kv2) buildServer(
 	// +defaultPath="/"
 	source *dagger.Directory,
 ) *dagger.File {
+	// TODO: multi arch
 	return m.devEnv(ctx, source, nil).
 		WithWorkdir("cmd/server").
 		WithExec([]string{"go", "build", "-o", "/app/server", "."}).
@@ -24,16 +25,10 @@ func (m *Kv2) BuildServerContainer(
 	// +defaultPath="/"
 	source *dagger.Directory,
 ) *dagger.Container {
-	version, err := m.svu(ctx, source).WithExec([]string{"svu", "current"}).Stdout(ctx)
-	if err != nil {
-		dag.Error(err.Error())
-	}
-
 	server := m.buildServer(ctx, source)
 	return dag.Container().
 		From("gcr.io/distroless/base-debian12").
 		WithLabel("org.opencontainers.image.title", "kv2").
-		WithLabel("org.opencontainers.image.version", version).
 		WithLabel("org.opencontainers.image.created", time.Now().String()).
 		WithLabel("org.opencontainers.image.source", "https://github.com/hugginsio/kv2").
 		WithLabel("org.opencontainers.image.licenses", "BSD-3-Clause").
--- dagger/forge.go
+++ dagger/forge.go
@@ -24,3 +24,22 @@ func (m *Kv2) PullRequest(
 
 	return strings.Join([]string{lint, test}, "\n"), nil
 }
+
+func (m *Kv2) Release(
+	ctx context.Context,
+	tag string,
+	registry string,
+	imageName string,
+	username string,
+	password *dagger.Secret,
+) error {
+	source := dag.Git("https://github.com/hugginsio/kv2.git", dagger.GitOpts{KeepGitDir: true}).Tag(tag).Tree()
+	serverContainer := m.BuildServerContainer(ctx, source).
+		WithLabel("org.opencontainers.image.version", tag)
+
+	serverContainer.
+		WithRegistryAuth(registry, username, password).
+		Publish(ctx, registry+"/"+imageName)
+
+	return nil
+}
