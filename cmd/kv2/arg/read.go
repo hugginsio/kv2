4a621ec06325abe8afdfe5dbcf836e43386a40c4
Kyle Huggins <kyle@huggins.io>
2025-04-02 07:34:23 -0500

    feat: complete kv2 read

--- cmd/kv2/arg/read.go
+++ cmd/kv2/arg/read.go
@@ -1,10 +1,16 @@
+// Copyright 2025 Kyle Huggins
+// SPDX-License-Identifier: BSD-3-Clause
+
 package arg
 
 import (
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"os"
 
 	"git.huggins.io/kv2/api"
+	"git.huggins.io/kv2/internal/cli"
 	"github.com/spf13/cobra"
 )
 
@@ -12,16 +18,36 @@ var readCmd = &cobra.Command{
 	Use:   "read <secret>",
 	Short: "Read the latest version of a secret",
 	Args:  cobra.ExactArgs(1),
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if cmd.Flag("decode").Changed && cmd.Flag("json").Changed {
+			return fmt.Errorf("--decode and --json are mutually exclusive")
+		}
+
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		res, err := kv2.Read(api.ReadSecretRequest{Key: args[0]})
 		if err != nil {
-			panic(err)
+			cli.PrintErrorOutput(jsonOutput, err)
+			os.Exit(1)
 		}
 
-		json.NewEncoder(os.Stdout).Encode(res)
+		if jsonOutput {
+			json.NewEncoder(os.Stdout).Encode(res)
+			return
+		}
+
+		if cmd.Flag("decode").Value.String() == "true" {
+			fmt.Println(string(res.Value))
+			return
+		}
+
+		fmt.Println(base64.StdEncoding.EncodeToString(res.Value))
 	},
 }
 
 func init() {
+	readCmd.Flags().BoolP("decode", "d", false, "attempt to decode value base64")
+
 	rootCmd.AddCommand(readCmd)
 }
