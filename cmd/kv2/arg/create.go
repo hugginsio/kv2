aad02bacaed194f0fc7fbb3bba704735647ddf8d
Kyle Huggins <kyle@huggins.io>
2025-04-05 20:31:15 -0500

    feat: cli create

--- cmd/kv2/arg/create.go
+++ cmd/kv2/arg/create.go
@@ -6,6 +6,9 @@ package arg
 import (
 	"os"
 
+	"connectrpc.com/connect"
+	secretsv1 "git.huggins.io/kv2/api/secrets/v1"
+	"git.huggins.io/kv2/internal/cli"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +17,28 @@ var createCmd = &cobra.Command{
 	Short: "Create a new secret",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		os.Exit(1)
+		req := &secretsv1.CreateSecretRequest{
+			Key: args[0],
+		}
+
+		if val, _ := cmd.Flags().GetString("from-literal"); val != "" {
+			req.Value = []byte(val)
+		}
+
+		if val, _ := cmd.Flags().GetString("from-file"); val != "" {
+			bytes, err := os.ReadFile(val)
+			if err != nil {
+				cli.PrintErrorOutput(jsonOutput, err)
+				os.Exit(1)
+			}
+
+			req.Value = bytes
+		}
+
+		if _, err := kv2.CreateSecret(cmd.Context(), &connect.Request[secretsv1.CreateSecretRequest]{Msg: req}); err != nil {
+			cli.PrintErrorOutput(jsonOutput, err)
+			os.Exit(1)
+		}
 	},
 }
 
