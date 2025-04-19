fa6783e3c4698adeb9a5e9be3b96cc5f238fca69
Kyle Huggins <kyle@huggins.io>
2025-04-19 15:34:28 -0500

    feat: scan for vulns with osv

--- dagger/ci.go
+++ dagger/ci.go
@@ -22,3 +22,17 @@ func (m *Kv2) Test(
 ) (string, error) {
 	return m.devEnv(ctx, source, nil).WithExec([]string{"go", "test", "-v", "./..."}).Stdout(ctx)
 }
+
+// Scan for vulnerabilities and license violations.
+func (m *Kv2) Scan(
+	ctx context.Context,
+	// +defaultPath="/"
+	source *dagger.Directory,
+) (string, error) {
+	return dag.Container().
+		From("ghcr.io/google/osv-scanner:v2.0.1").
+		WithWorkdir("/go/src/").
+		WithMountedDirectory("/go/src/", source).
+		WithExec([]string{"/osv-scanner", "scan", "--call-analysis=go", "-r", "/go/src/go.mod"}).
+		Stdout(ctx)
+}
